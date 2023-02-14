b29753faeb8ccc0ecb12868557618e131e33d2b2
oliver halvey <oliverhalvey310@yahoo.com>
2023-02-14 07:29:35 -0800

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Oliver Halvey on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
