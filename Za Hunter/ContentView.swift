37f7f353365cce009233560fdbd64943b6c83a59
oliver halvey <oliverhalvey310@yahoo.com>
2023-02-16 07:29:33 -0800

    Added map search feature

--- Za Hunter/ContentView.swift
+++ Za Hunter/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import MapKit
 struct ContentView: View {
+    @State private var places = [Place]()
     @StateObject var locationManager = LocationManager()
         @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var region = MKCoordinateRegion( //MK = MapKit
@@ -23,13 +24,62 @@ struct ContentView: View {
                        coordinateRegion: $region,
                        interactionModes: .all,
                        showsUserLocation: true,
-                       userTrackingMode: $userTrackingMode
-                   )
+                       userTrackingMode: $userTrackingMode,
+                       annotationItems: places) { place in
+                           MapPin(coordinate: place.annotation.coordinate)
+                       }
+                       .onAppear(perform: {
+                           performSearch(item: "Pizza")
+                       })
+    }
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+
+                searchRequest.naturalLanguageQuery = item
+
+                searchRequest.region = region
+
+                let search = MKLocalSearch(request: searchRequest)
+        search.start { response, error in
+            if let response = response {
+                for mapItem in response.mapItems {
+                                   let annotation = MKPointAnnotation()
+                                   annotation.coordinate = mapItem.placemark.coordinate
+                                   annotation.title = mapItem.name
+                                   places.append(Place(annotation: annotation, mapItem: mapItem))
+
+                               }
+                       }
+        }
+
+       }
+    struct Marker: View {
+        var mapItem: MKMapItem
+
+        var body: some View {
+
+            if let url = mapItem.url {
+
+                Link(destination: url, label: {
+
+                    Image("pizza")
+
+                })
+
+            }
+
+        }
+
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct Place: Identifiable {
+    let id = UUID()
+    let annotation: MKPointAnnotation
+    let mapItem: MKMapItem
+
 }
